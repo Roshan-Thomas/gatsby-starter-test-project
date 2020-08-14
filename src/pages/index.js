6fb9aa5e8a284c1a5c5d0e9738b13021d222b21a
Roshan Thomas <roshan.thomas@qatar.tamu.edu>
2020-08-14 19:37:37 +0300

    added Login/Signup using Netlify Identity

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,18 +5,42 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+function initNetlifyIdentity() {
+  console.log("initNetlifyIdentity called. ")
+  const script = document.createElement("script");
 
+  script.src = "https://identity.netlify.com/v1/netlify-identity-widget.js"
+  script.async = true;
+
+  document.body.appendChild(script);
+}
+
+function openNetlifyModal() {
+  const netlifyIdentity = window.netlifyIdentity;
+
+  if(netlifyIdentity)
+    netlifyIdentity.open();
+  else
+    console.log('netlifyIdentity not defined')
+
+}
+
+const IndexPage = () => {
+  initNetlifyIdentity();
+
+  return(
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <h2 onClick={() => { openNetlifyModal() }}>Login</h2>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    </Layout>
+  )
+}
 export default IndexPage
